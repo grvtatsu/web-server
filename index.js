3fe522385bbdd56e27ef3ca78b38587dedd6d545
grvtatsu <t.otsuki.grv@gmail.com>
2022-03-28 12:18:34 +0900

    First commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const path = require("path");
+const app = express();
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, "public")));
+
+// app.get('/', function (req, res) {
+// 	console.log(req);
+//   res.send('<h1>Hello World!!!</h1>');
+// })
+
+app.get('/about', function (req, res) {
+	res.send('about');
+})
+
+app.post('/api/v1/quiz', function (req, res) {
+	const answer = req.body.answer;
+	if(answer === "2"){
+		// res.send("true");
+		res.redirect("/correct.html");
+	}else{
+		// res.send("false");
+		res.redirect("/wrong.html");
+	}
+})
+
+app.get('/api/v1/users', function (req, res) {
+	res.send({
+		name: "Mike",
+		age: 30,
+	});
+})
+
+
+app.listen(3000, function(){
+	console.log("running");
+})
